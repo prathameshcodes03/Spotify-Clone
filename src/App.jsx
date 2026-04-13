814914f0edcebf0138ad590c475b608b531ceb3c
prathameshcodes03 <prathamesh.zone09@gmail.com>
2026-04-13 22:16:21 +0530

    added template

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-600 h-60 flex items-center justify-center'>
+      App
+    </div>
+  )
+}
+
+export default App
 
 
