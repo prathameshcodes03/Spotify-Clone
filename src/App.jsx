e6bc5104fa948e46ee7b0b2458dc9d71e766a51b
prathameshcodes03 <prathamesh.zone09@gmail.com>
2026-04-18 19:08:30 +0530

    added login jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes,Route,Link,Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
-    <div className='bg-amber-600 h-60 flex items-center justify-center'>
-      App
-    </div>
+
+    <Routes>
+            <Route path='/' element={<Navigate to='/login'/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+  
+
   )
 }
 
